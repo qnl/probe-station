9672496b2383b099b00dde35a7382ca07f790650
Larry Chen <larrychen@berkeley.edu>
2020-09-18 16:13:01 -0700

    Reformatted documentation

--- P200L.py
+++ P200L.py
@@ -1,22 +1,22 @@
-'''module to control the probe station movement for the QNL probe station in fab.
-See NetProbe7 Manual for command information
-'''
+"""Module to control the probe station movement.
 
+See NetProbe7 Manual for command information
+"""
 #added delay in send_command to as an idea to help reduce runtime errors
 import socket
 import time
 
 class P200L():
-    '''Class to interface with the probe station. Uses TCP/IP.'''
+    """Class to interface with the probe station. Uses TCP/IP."""
     
     def __init__(self,ip='192.168.0.6',port=9002):
-        '''Create a probe station interface.
-        Args:
-            ip: IP address found in the 'Router' application
-            port: port found in the 'Router' application
-            verbose: print interesting info 
-        '''
+        """Create a probe station interface.
 
+        Args:
+            ip: IP address found in the "Router" application.
+            port: port found in the "Router" application.
+            verbose: print interesting info.
+        """
 
         self.ip = ip
         self.port = port
@@ -24,17 +24,23 @@ class P200L():
         self.socket.connect((self.ip, self.port))
     
     def reconnect(self):
-        '''Reconnect to the server. Use if the server is restarted
-        or the connection somehow else went bad.'''
+        """Reconnects to the server. 
+        
+        Use if the server is restarted or the connection somehow else went bad.
+        """
         self.socket.close()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.ip, self.port))
     
     def send_command(self, message):
-        '''Send a command to the probe station.
+        """Send a command to the probe station.
+        
         Args:
-            message: command to send to the probe station as found in the manual
-        Returns: Each response may be interpreted as shown in the probe station manual'''
+            message: command to send to the probe station as found in the manual.
+
+        Returns: 
+            Each response may be interpreted as shown in the probe station manual.
+        """
         try:
             self.socket.send(str.encode(message+'\r'))
             time.sleep(0.05)
@@ -63,65 +69,81 @@ class P200L():
         return retval
     
     def goto_die(self, x, y):
-        '''Move to a certain die location indexed by x and y
-        '''
+        """Move to a certain die location indexed by x and y.
+        """
         self.send_command(f':WFR:MOV:CR {x} {y}')
         self.send_command(':PRB:REFXY 0 0')
 
     def get_die(self):
-        '''return the index x y values of the current die
-        '''
+        """Return the index values of the current die.
+
+        Returns:
+            The index values `(x, y)` of the current die.
+        """
         x,y = self.send_command(f':WFR:POS:CR?').split(' ')
         return int(x),int(y)
 
     def reset_die(self):
-        '''reset all dies to untested in the wafer Map
-        '''
+        """Reset all dies to untested in the wafer Map.
+
+        """
         self.send_command(f':WFR:RESET')
     
     def goto_first_die(self):
-        '''Move to the first die in the probe sequence.
-        Returns: (x,y) index of this die
-        '''
+        """Move to the first die in the probe sequence.
+        
+        Returns:
+            The index `(x,y)` of this die.
+        """
+
         x,y = self.send_command(f':WFR:FIRST?').split(' ')
         self.send_command(':PRB:REFXY 0 0')
         return int(x),int(y)
 
     def goto_same_die(self):
-        '''Move to the same die.
-        Returns: (x,y) index of this die
-        '''
+        """Move to the same die.
+        
+        Returns: 
+            The index `(x,y)` of this die.
+        """
         x,y = self.send_command(f':WFR:POS:CR?').split(' ')
         self.send_command(':PRB:REFXY 0 0')
         return int(x),int(y)
     
     def goto_next_die(self):
-        '''Move to the next die.
-        Returns: (x,y) index of this die
-        '''
+        """Move to the next die.
+        
+        Returns:
+            The index `(x,y)` of this die
+        """
         x,y = self.send_command(f':WFR:NEXT?').split(' ')
         self.send_command(':PRB:REFXY 0 0')
         return int(x),int(y)
     
     def goto_subsite(self, subsite):
-        '''Move to a subsite of the current die
+        """Move to a subsite of the current die/
+        
         Args:
-            subsite: index of the subsite to goto.
-                current subsite x,y coordinate visible
-                in P200L Navigator'''
+            subsite: Index of the subsite to go to. The current subsite `(x,y)`
+                coordinate is visible in P200L Navigator
+        """
         self.send_command(f':SUB:GOTO {subsite}')
 
     def goto_next_subsite(self):
-        '''Move to the next subsite.
-        Returns: new subsite index. returns -1 if there there
-            no more subsites'''
+        """Move to the next subsite.
+        
+        Returns: 
+            New subsite index. Returns -1 if there there no more subsites left.
+        """
         result = self.send_command(':SUB:NEXT?').split(' ')
         return int(result[0])
 
     def auto_raise(self, do_raise=None):
-        '''set/get auto raising
-        Args: do_raise (bool)
-        '''
+        """Set/get auto raising.
+        
+        Args: 
+            do_raise (bool): Whether or not to raise the probe tips.
+        """
         retval = self.send_command(':PRB:DNM?')
         auto_z_flags = retval.split(' ')
         if do_raise is not None:
@@ -133,9 +155,11 @@ class P200L():
         return {'True':True,'False':False}[auto_z_flags[0]]
 
     def auto_lower(self, do_lower=None):
-        '''set/get auto lowering
-        Args: do_lower (bool)
-        '''
+        """Set/get auto lowering.
+        
+        Args: 
+            do_lower (bool): Whether or not to lower the probe tips.
+        """
         
         retval = self.send_command(':PRB:DNM?')
         auto_z_flags = retval.split(' ')
@@ -148,36 +172,51 @@ class P200L():
         return {'True':True,'False':False}[auto_z_flags[1]]
 
     def use_pattern_recognition(self, do_rec=None):
-        '''get/set using pattern recognition. If True, every time the stage moves to
-        a new die, the probe station will use pattern recognition for local alignment'''
+        """Get/set using pattern recognition. 
+        
+        Args:
+            do_rec (bool): If `True`, every time the stage moves to a new die, 
+                the probe station will use pattern recognition for local
+                alignment.
+        """
         if do_rec is not None:
             do_rec = {True:"true",False:"false"}[do_rec]
             retval = self.send_command(f':WFR:PATREC {do_rec}')
         return {"True":True, "False":False}[self.send_command(f':WFR:PATREC?')]
 
     def load_wafer_file(self, wafer_file):
-        '''Load a wafer map file to the probe station. see manual for
-        file data structure
-        Args: wafer_file: file name to load
-        '''
+        """Load a wafer map file to the probe station. 
+        
+        See manual for file data structure
+
+        Args:
+            wafer_file: File name to load.
+        """
         self.send_command(f':WFR:FILE {subsite_file}')
 
     def load_subsite_file(self, subsite_file):
-        '''Load a subsite file to the probe station. see manual for
-        file data structure
-        Args: wafer_file: file name to load
-        '''
+        """Load a subsite file to the probe station. 
+        
+        See manual for file data structure.
+        
+        Args:
+            wafer_file: File name to load.
+        """
         self.send_command(f':SUB:FILE {subsite_file}')
 
     def wafer_array_shape(self):
-        '''Return the shape of the wafer array. returns as (col, row).'''
+        """Return the shape of the wafer array.
+        
+        Returns:
+            A tuple `(col, row)`.
+        """
 
         # Note that the real command ':WFR:MAP:CR?' is broken
         retval = list(map(lambda x:x.split(','),self.send_command(':WFR:MAP:CSV?').split('~')))
         return int(retval[4][1]), int(retval[3][1])
 
     def wafer_array_shape(self):
-        '''Return the size of the subsite array.'''
+        """Returns the size of the subsite array."""
 
         retval = list(map(lambda x:x.split(','),self.send_command(':WFR:MAP:CSV?').split('~')))
         return int(retval[4][1]), int(retval[3][1])
--- Probe_Measurement.py
+++ Probe_Measurement.py
@@ -1,30 +1,39 @@
-'''Class to take probe measurements for the QNL probe station in fab.
-This is a top level class you want to instantiate for taking measurements
-'''
+"""Micromanipulator P200L Probe Station
+
+This is a module for taking automated measurements with the probe station.
+"""
 
 from time import sleep, time
+
+import datetime as dt
 
 from probe_station.P200L import P200L
 from probe_station.SR810_lockin import SR810_lockin
 
 class Probe_Measurement():
-    '''class to handle probe station measurements
-    '''
+    """Measurement class.
+    """
+
     def __init__(self,
                  wafer_file=None,
                  subsite_file=None,
                  p200_ip='192.168.0.6',
                  p200_port=9002,
                  reset_wafer_map=True):
-        '''Instantiate a measurement. Connects to the probe station and the lockin.
-        Data from the meaasurement are stored in self.data
+        """Instantiate a measurement. 
+        
+        Connects to the probe station and the lockin. Data from the meaasurement
+        is stored in `self.data`.
 
-        Args: wafer_file: wafer to load. None to keep current wafer file.
-              subsite_file: subsite_file to load. None to keep current subsite_file file.
-              p200_ip: ip address found in the "Router" application
-              p200_port: port found in the "Router" application
-              reset_wafer_map: reset the wafer map so that all sites are untested
-        '''
+        Args: 
+            wafer_file: Wafer to load. `None` to keep current wafer file.
+            subsite_file: Subsite file to load. `None` to keep current 
+                `subsite_file` file.
+            p200_ip: IP address found in the "Router" application
+            p200_port: Port found in the "Router" application
+            reset_wafer_map: Reset the wafer map so that all sites are
+                untested.
+        """
         
         self.p200 = P200L(ip=p200_ip,port=p200_port)
         self.lockin = SR810_lockin()
@@ -38,17 +47,24 @@ class Probe_Measurement():
         self.reset_data_array(reset_wafer_map=reset_wafer_map)
     
     def reset_data_array(self, reset_wafer_map=True):
-        '''Reset data and update the size of the data array to reflect
-        the number of die on the wafer. Optinally resets the tested dies in the wafer map
+        """Reset data.
+        
+        Resets data and updates the size of the data array to reflect the number
+        of dies on the wafer. Optionally resets the tested dies in the wafer
+        map.
+
         Args:
             reset_wafer_map: reset the wafer map so that all sites are untested
-        '''
+        """
+
         (x_count, y_count) = self.p200.wafer_array_shape()
         self.data = [[[] for _ in range(y_count)] for _ in range(x_count)]
         self.p200.reset_die()
         
     def probe_die(self, subsites=True):
-        '''Probe the current die and update the probe value data array'''
+        """Probe the current die and update the probe value data array.
+        """
+        
         x_i, y_i = self.p200.get_die()
         print(f'Starting die. x:{x_i}, y:{y_i}')
         tic = time()
@@ -68,19 +84,24 @@ class Probe_Measurement():
         	self.data[x_i][y_i].append(self.lockin.voltage_in())
         	print(f'Die time {round((time()-tic)/60,2)} minutes')
 
-    def probe_wafer(self, reset_die=True, use_pattern_recognition=True, subsites=True):
-        '''Probe a full wafer. Assumes that the user has adjusted the probe tips and set
-        the down position such that the tips will contact probe points when they land.
-        Also assumes that the wafer map and sub file are accurate.
+    def probe_wafer(
+        self, reset_die=True, use_pattern_recognition=True, subsites=True
+    ):
+        """Probe a full wafer. 
+        
+        Assumes that the user has adjusted the probe tips and set the down 
+        position properly such that the tips will contact probe points when they
+        land. Also assumes that the wafer map and sub file are accurate.
+
         Args:
-            reset_die: True to reset the data array and start from the beginning.
-                False to continue from where the wafer was left off.
-            use_pattern_recognition: use pattern recognition for local alignment when
-                the probe station moves to a new die.
-            substies: if only using 1 subsite per die location, set to false and put x=0,y=0 
-            	for subsite 1 in P200L navigator. 
-        '''
-        start_time = time()
+            reset_die: `True` to reset the data array and start from the 
+                beginning. `False` to continue from where the wafer was left off.
+            use_pattern_recognition: Use pattern recognition for local alignment
+                when the probe station moves to a new die.
+            subsites: If only using 1 subsite per die location, set to false and
+                put `x=0,y=0` for subsite 1 in P200L navigator. 
+        """
+        start_time = dt.datetime.now()
         self.p200.auto_raise(True)
         self.p200.auto_lower(True)
         self.p200.use_pattern_recognition(use_pattern_recognition)
@@ -96,12 +117,21 @@ class Probe_Measurement():
                 self.probe_die(subsites=subsites)
                 x_index, y_index = self.p200.goto_next_die()
         except KeyboardInterrupt:
-            print(f'KeyboardInterrupt detected. Total time: {round((time()-start_time)/60,2)} minutes')
+            print(
+                f'KeyboardInterrupt detected. '
+                f'Total time: {round((time()-start_time)/60,2)} minutes'
+            )
             self.p200.send_command(f':WFR:BIN {x_index} {y_index} T')
             raise
         except:
-            print(f'Error. Total time: {round((time()-start_time)/60,2)} minutes')
+            print(
+                f'Error. Total time: {round((time()-start_time)/60,2)} minutes'
+            )
             raise
-        print(time())
-        print(start_time)
-        print(f'Wafer Done. Total time: {round((time()-start_time)/60,2)} minutes')
+
+        end_time = dt.datetime.now()
+        print(f'Started at {start_time}')
+        print(f'Finished at {end_time}')
+        print(
+            f'Wafer Done. Total time: {end_time - start_time}'
+        )
